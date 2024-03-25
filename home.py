99b53c9c4359d7626af42bd58fc5929faff5a84e
Rahul Nandakumar <68471883+rahulnkm@users.noreply.github.com>
2024-03-25 14:48:15 -0400

    store and write messages

--- home.py
+++ home.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.text_area("Write something")
+input = st.text_area("Write something")
+
+messages = []
+
+if input:
+    messages.append(input)
+
+
+display = st.button("Show messages")
+
+if display:
+    st.write(messages)
